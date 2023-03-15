f055cd7a0cbf3c2401eb21f6ac7a1ba558ed169d
ithallomendes <74620052+ithallomendes@users.noreply.github.com>
2023-03-15 12:34:37 -0300

    first commit

---
+++ resources/views/filial/show.blade.php
@@ -0,0 +1,41 @@
+<<<<<<< HEAD
+@extends('adminlte::page')
+
+@section('title', 'InventoryCred')
+
+@section('content_header')
+    <h1>Inicio</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+=======
+@extends('adminlte::page')
+
+@section('title', 'InventoryCred')
+
+@section('content_header')
+    <h1>Inicio</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+>>>>>>> 4285e2f (Segundo Commit Teste - Alteração EntradaProduto)
